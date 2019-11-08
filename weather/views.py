ebc3d8c0d7e717474f9628d1f3c687d23dc7bc57
Phuc Le <you@example.com>
2019-11-08 15:36:14 -0800

    Able to get realtime weather report of Seattle

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,20 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request, 'weather/weather.html')
+  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=b843deeef7e0bc94223e723366ed9d94'
+  city = 'Seattle'
+
+  r = requests.get(url.format(city)).json()
+
+  city_weather = {
+    'city' : city,
+    'temperature': r['main']['temp'],
+    'description': r['weather'][0]['description'],
+    'icon': r['weather'][0]['icon'],
+  }
+
+  context = {'city_weather': city_weather
+  }
+  return render(request, 'weather/weather.html', context )
