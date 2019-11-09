3b26dfb615f22ace0b04a93920194436419fc8a8
Phuc Le <you@example.com>
2019-11-08 16:56:11 -0800

    Box to add mutiple cities

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,17 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import Cityform
 # Create your views here.
 def index(request):
   url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=b843deeef7e0bc94223e723366ed9d94'
   city = 'Seattle '
+
+  if request.method == 'POST':
+    form = Cityform(request.POST)
+    form.save()
+
+  form = Cityform()
 
   weather_data = []
   cities = City.objects.all()
@@ -18,7 +25,8 @@ def index(request):
       'icon': r['weather'][0]['icon'],
     }
     weather_data.append(city_weather)
-  print(weather_data)
-  context = {'weather_data': weather_data
+
+  context = {'weather_data': weather_data,
+  'form': form 
   }
   return render(request, 'weather/weather.html', context )
